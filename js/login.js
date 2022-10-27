5ff2652bff9c09298973d4bd74c14deb7fbdd8bf
Wesley Azevedo <azevedosou@gmail.com>
2022-10-27 19:43:08 -0300

    set localStorage

--- js/login.js
+++ js/login.js
@@ -1,8 +1,17 @@
-function acessar() {
-    const usuario = document.getElementById("usuario");
-    const senha = document.getElementById("senha");
+function insertCredential() {
+    localStorage.setItem('user', "login");
+    localStorage.setItem('pass', "senha");
+    console.log(localStorage.getItem('user'))
+    console.log(localStorage.getItem('senha'))   
+ 
 
-    console.log(usuario.value+senha.value)
+}
 
-    
+function access() {
+    if (user.value === localStorage.getItem('user').valeu && pass.value === "senha") {
+        localStorage.setItem("logged", true);
+        window.location.href = "index.html"
+    } else {
+        loginMessage.innerHTML = 'Tente usar "login" e "senha".'
+    }
 }
