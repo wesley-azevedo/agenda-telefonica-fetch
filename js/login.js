10d08c28f474b1bd4943008001e7bf2b1210d7be
Wesley Azevedo <azevedosou@gmail.com>
2022-10-27 15:45:49 -0300

    inicio javascript login

--- js/login.js
+++ js/login.js
@@ -1,1 +1,8 @@
-console.log("chamou script")
+function acessar() {
+    const usuario = document.getElementById("usuario");
+    const senha = document.getElementById("senha");
+
+    console.log(usuario.value+senha.value)
+
+    
+}
