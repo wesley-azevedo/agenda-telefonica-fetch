a3f5dbbd7c2dbe297b5a58e0c83ff0b0c2f6e409
wesley-azevedo <azevedosou@gmail.com>
2022-10-29 15:06:10 -0300

    inclui a funcao de buscar

--- js/application.js
+++ js/application.js
@@ -2,11 +2,25 @@ contactList = JSON.parse(localStorage.getItem('contactList'))
 console.log(contactList)
 contactFilter = JSON.parse(localStorage.getItem('contactFilter'))
 console.log(contactFilter)
+order = localStorage.getItem('order')
+
+function orderList() {
+    if (order == "asc") {
+        contactList.sort(function(a,b) {
+            return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+        });
+    } else {
+        "reverse()"
+    }
+    escreverHTML();
+}
 
 function checkCredential() {
     if (localStorage.getItem('logged') === false) {
         window.location.href = "login.html"
     } else {
+        localStorage.setItem('order', "asc")
+        updateContacts()
         escreverHTML();
     }
 }
@@ -17,18 +31,17 @@ function logout() {
 }
 
 async function updateContacts() {
-    let response = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3');
+    let response = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa');
     let body = await response.json();
     localStorage.setItem('contactList', JSON.stringify(body))
     contactList = JSON.parse(localStorage.getItem('contactList'))
     console.log(contactList)
-    escreverHTML()
 }
 
 async function addContact() {
     let addName = inputAddName.value
     let addPhone = inputAddPhone.value
-    let addNewContact = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3', {
+    let addNewContact = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa', {
         method: "POST",
         headers: {
             'content-type': 'application/json'
@@ -49,77 +62,72 @@ async function addContact() {
 function escreverHTML() {
 
     if (contactFilter != [0]) {
-        numberOfContacts.innerHTML = contactList.length
-        contactList.forEach(item => {
-        let contact = document.createElement("tr");
-        contact.id = `contact${item.id}`;
-        document.querySelector('#contactsBody').appendChild(contact);
-
-        let id = document.createElement("td");
-        id.innerHTML = `${item.id}`;
-        document.querySelector(`#${contact.id}`).appendChild(id);
-
-        let name = document.createElement("td");
-        name.innerHTML = `${item.nome}`;
-        document.querySelector(`#${contact.id}`).appendChild(name);
-
-        let phone = document.createElement("td");
-        phone.innerHTML = `${item.idade}`;
-        document.querySelector(`#${contact.id}`).appendChild(phone);
-
-        let options = document.createElement("td");
-        options.id = `options${item.id}`;
-        document.querySelector(`#${contact.id}`).appendChild(options);
-
-        let editBtn = document.createElement("input");
-        editBtn.type = 'button'
-        editBtn.value = 'editar'
-        document.querySelector(`#${options.id}`).appendChild(editBtn);
-
-        let deleteBtn = document.createElement("input");
-        deleteBtn.type = 'button'
-        deleteBtn.value = 'excluir'
-        document.querySelector(`#${options.id}`).appendChild(deleteBtn);
-    })  
-    
-    
-
-
-
-    } else {
         numberOfContacts.innerHTML = contactFilter.length
         contactFilter.forEach(item => {
-        let contact = document.createElement("tr");
-        contact.id = `contact${item.id}`;
-        document.querySelector('#contactsBody').appendChild(contact);
+            let contact = document.createElement("tr");
+            contact.id = `contact${item.id}`;
+            document.querySelector('#contactsBody').appendChild(contact);
 
-        let id = document.createElement("td");
-        id.innerHTML = `${item.id}`;
-        document.querySelector(`#${contact.id}`).appendChild(id);
+            let id = document.createElement("td");
+            id.innerHTML = `${item.id}`;
+            document.querySelector(`#${contact.id}`).appendChild(id);
 
-        let name = document.createElement("td");
-        name.innerHTML = `${item.nome}`;
-        document.querySelector(`#${contact.id}`).appendChild(name);
+            let name = document.createElement("td");
+            name.innerHTML = `${item.nome}`;
+            document.querySelector(`#${contact.id}`).appendChild(name);
 
-        let phone = document.createElement("td");
-        phone.innerHTML = `${item.idade}`;
-        document.querySelector(`#${contact.id}`).appendChild(phone);
+            let phone = document.createElement("td");
+            phone.innerHTML = `${item.idade}`;
+            document.querySelector(`#${contact.id}`).appendChild(phone);
 
-        let options = document.createElement("td");
-        options.id = `options${item.id}`;
-        document.querySelector(`#${contact.id}`).appendChild(options);
+            let options = document.createElement("td");
+            options.id = `options${item.id}`;
+            document.querySelector(`#${contact.id}`).appendChild(options);
 
-        let editBtn = document.createElement("input");
-        editBtn.type = 'button'
-        editBtn.value = 'editar'
-        document.querySelector(`#${options.id}`).appendChild(editBtn);
+            let editBtn = document.createElement("input");
+            editBtn.type = 'button'
+            editBtn.value = 'editar'
+            document.querySelector(`#${options.id}`).appendChild(editBtn);
 
-        let deleteBtn = document.createElement("input");
-        deleteBtn.type = 'button'
-        deleteBtn.value = 'excluir'
-        document.querySelector(`#${options.id}`).appendChild(deleteBtn);
-    })
-    } 
+            let deleteBtn = document.createElement("input");
+            deleteBtn.type = 'button'
+            deleteBtn.value = 'excluir'
+            document.querySelector(`#${options.id}`).appendChild(deleteBtn);
+        })
+    } else {
+        numberOfContacts.innerHTML = contactList.length
+        contactList.forEach(item => {
+            let contact = document.createElement("tr");
+            contact.id = `contact${item.id}`;
+            document.querySelector('#contactsBody').appendChild(contact);
+
+            let id = document.createElement("td");
+            id.innerHTML = `${item.id}`;
+            document.querySelector(`#${contact.id}`).appendChild(id);
+
+            let name = document.createElement("td");
+            name.innerHTML = `${item.nome}`;
+            document.querySelector(`#${contact.id}`).appendChild(name);
+
+            let phone = document.createElement("td");
+            phone.innerHTML = `${item.idade}`;
+            document.querySelector(`#${contact.id}`).appendChild(phone);
+
+            let options = document.createElement("td");
+            options.id = `options${item.id}`;
+            document.querySelector(`#${contact.id}`).appendChild(options);
+
+            let editBtn = document.createElement("input");
+            editBtn.type = 'button'
+            editBtn.value = 'editar'
+            document.querySelector(`#${options.id}`).appendChild(editBtn);
+
+            let deleteBtn = document.createElement("input");
+            deleteBtn.type = 'button'
+            deleteBtn.value = 'excluir'
+            document.querySelector(`#${options.id}`).appendChild(deleteBtn);
+        })
+    }
 }
 
 function searchContact() {
@@ -154,7 +162,7 @@ async function atualizar(identificador) {
     let nomeNovo = prompt("nome?")
     let idadeNovo = prompt("idade?")
 
-    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/' + identificador, {
+    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa/' + identificador, {
         method: 'PUT',
         headers: {
             'content-type': 'application/json'
@@ -174,7 +182,7 @@ async function atualizar(identificador) {
 }
 
 async function deletar(identificador) {
-    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/' + identificador, {
+    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa/' + identificador, {
         method: 'DELETE',
     });
     if (res.ok) {
@@ -183,3 +191,6 @@ async function deletar(identificador) {
         console.log(res.statusText)
     }
 }
+
+
+
