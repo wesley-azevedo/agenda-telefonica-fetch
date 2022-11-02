a399fac85947907fb79376e20e3865333af7a3ff
Wesley Azevedo <azevedosou@gmail.com>
2022-11-02 11:01:40 -0300

    correcao bug inicializacao e otimizacao do codigo

--- js/application.js
+++ js/application.js
@@ -10,6 +10,7 @@ function checkCredential() {
         if (localStorage.getItem('favoriteList') != null) {
             favoriteList = JSON.parse(localStorage.getItem('favoriteList'));
         }
+
         writeHTML();
     }
 }
@@ -52,120 +53,74 @@ async function writeHTML() {
 
     let response = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa');
     let body = await response.json();
-    localStorage.setItem('contactList', JSON.stringify(body.sort(function(a,b) {
+    localStorage.setItem('contactList', JSON.stringify(body.sort(function (a, b) {
         return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
     })));
-    contactList = JSON.parse(localStorage.getItem('contactList'));
-    if (localStorage.getItem('order') != "asc") {
-        contactList = JSON.parse(localStorage.getItem('contactList')).reverse();
-    }
 
-    if (contactFilter != [0]) {
-        numberOfContacts.innerHTML = contactFilter.length;
-        contactFilter.forEach(item => {
-            let contact = document.createElement("tr");
-            contact.id = `contact${item.id}`;
-            document.querySelector('#contactsBody').appendChild(contact);
-
-            let id = document.createElement("td");
-            id.id = `id${item.id}`;
-            document.querySelector(`#${contact.id}`).appendChild(id);
-            let starIcon = document.createElement("i");
-            starIcon.id = `starIcon${item.id}`;
-            if (favoriteList.includes(item.id)) {
-                starIcon.classList = "bi bi-star";                
-            } else {
-                starIcon.classList = "bi bi-star-fill";
-            }
-            starIcon.addEventListener("click", () => {favoriteContact(item.id)});
-            document.querySelector(`#id${item.id}`).appendChild(starIcon); 
-
-            let name = document.createElement("td");
-            name.innerHTML = `${item.nome}`;
-            document.querySelector(`#${contact.id}`).appendChild(name);
-
-            let phone = document.createElement("td");
-            phone.innerHTML = `${item.idade}`;
-            document.querySelector(`#${contact.id}`).appendChild(phone);
-
-            let options = document.createElement("td");
-            options.id = `options${item.id}`;
-            document.querySelector(`#${contact.id}`).appendChild(options);
-
-            let editBtn = document.createElement("button");
-            editBtn.id = `edit${item.id}`;
-            editBtn.type = 'button';
-            editBtn.addEventListener("click", () => {updateContact(item.id)});
-            editBtn.classList = "btn btn-outline-warning m-1";  
-            document.querySelector(`#${options.id}`).appendChild(editBtn);
-            let editIcon = document.createElement("i");
-            editIcon.classList = "bi bi-pencil-square";
-            document.querySelector(`#edit${item.id}`).appendChild(editIcon);
-
-            let deleteBtn = document.createElement("button");
-            deleteBtn.id = `delete${item.id}`
-            deleteBtn.type = 'button';
-            deleteBtn.addEventListener("click", () => {deleteContact(item.id)});
-            deleteBtn.classList = "btn btn-outline-danger m-1";
-            document.querySelector(`#${options.id}`).appendChild(deleteBtn);
-            let deleteIcon = document.createElement("i");
-            deleteIcon.classList = "bi bi-trash";
-            document.querySelector(`#delete${item.id}`).appendChild(deleteIcon);
-            
-        })
+    if (contactFilter == 0 || contactFilter == null) {
+        contactList = JSON.parse(localStorage.getItem('contactList'));
     } else {
-        numberOfContacts.innerHTML = contactList.length;
-        contactList.forEach(item => {
-            let contact = document.createElement("tr");
-            contact.id = `contact${item.id}`;
-            document.querySelector('#contactsBody').appendChild(contact);
-
-            let id = document.createElement("td");
-            id.id = `id${item.id}`;
-            document.querySelector(`#${contact.id}`).appendChild(id);
-            let starIcon = document.createElement("i");
-            starIcon.id = `starIcon${item.id}`;
-            if (favoriteList.includes(item.id)) {
-                starIcon.classList = "bi bi-star";                
-            } else {
-                starIcon.classList = "bi bi-star-fill";
-            }
-            starIcon.addEventListener("click", () => {favoriteContact(item.id)});
-            document.querySelector(`#id${item.id}`).appendChild(starIcon); 
-
-            let name = document.createElement("td");
-            name.innerHTML = `${item.nome}`;
-            document.querySelector(`#${contact.id}`).appendChild(name);
-
-            let phone = document.createElement("td");
-            phone.innerHTML = `${item.idade}`;
-            document.querySelector(`#${contact.id}`).appendChild(phone);
-
-            let options = document.createElement("td");
-            options.id = `options${item.id}`;
-            document.querySelector(`#${contact.id}`).appendChild(options);
-
-            let editBtn = document.createElement("button");
-            editBtn.id = `edit${item.id}`
-            editBtn.type = 'button';
-            editBtn.addEventListener("click", () => {updateContact(item.id)});
-            editBtn.classList = "btn btn-outline-warning m-1";
-            document.querySelector(`#${options.id}`).appendChild(editBtn);
-            let editIcon = document.createElement("i");
-            editIcon.classList = "bi bi-pencil-square";
-            document.querySelector(`#edit${item.id}`).appendChild(editIcon);
-        
-            let deleteBtn = document.createElement("button");
-            deleteBtn.id = `delete${item.id}`;
-            deleteBtn.type = 'button';
-            deleteBtn.addEventListener("click", () => {deleteContact(item.id)});
-            deleteBtn.classList = "btn btn-outline-danger m-1";
-            document.querySelector(`#${options.id}`).appendChild(deleteBtn);
-            let deleteIcon = document.createElement("i");
-            deleteIcon.classList = "bi bi-trash";
-            document.querySelector(`#delete${item.id}`).appendChild(deleteIcon);
-        })
+        contactList = contactFilter
     }
+
+    if (localStorage.getItem('order') != "asc") {
+        contactList = contactList.reverse();
+    }
+
+    numberOfContacts.innerHTML = contactList.length;
+    contactList.forEach(item => {
+        let contact = document.createElement("tr");
+        contact.id = `contact${item.id}`;
+        document.querySelector('#contactsBody').appendChild(contact);
+
+        let id = document.createElement("td");
+        id.id = `id${item.id}`;
+        document.querySelector(`#${contact.id}`).appendChild(id);
+        let starIcon = document.createElement("i");
+        starIcon.id = `starIcon${item.id}`;
+        if (favoriteList.includes(item.id)) {
+            starIcon.classList = "bi bi-star";
+        } else {
+            starIcon.classList = "bi bi-star-fill";
+        }
+        starIcon.addEventListener("click", () => { favoriteContact(item.id) });
+        document.querySelector(`#id${item.id}`).appendChild(starIcon);
+
+        let name = document.createElement("td");
+        name.innerHTML = `${item.nome}`;
+        document.querySelector(`#${contact.id}`).appendChild(name);
+
+        let phone = document.createElement("td");
+        phone.innerHTML = `${item.idade}`;
+        document.querySelector(`#${contact.id}`).appendChild(phone);
+
+        let options = document.createElement("td");
+        options.id = `options${item.id}`;
+        document.querySelector(`#${contact.id}`).appendChild(options);
+
+        let editBtn = document.createElement("button");
+        editBtn.id = `edit${item.id}`;
+        editBtn.type = 'button';
+        editBtn.data = "modal"
+        editBtn.target = "#addContact"
+
+        editBtn.addEventListener("click", () => { updateContact(item.id) });
+        editBtn.classList = "btn btn-outline-warning m-1";
+        document.querySelector(`#${options.id}`).appendChild(editBtn);
+        let editIcon = document.createElement("i");
+        editIcon.classList = "bi bi-pencil-square";
+        document.querySelector(`#edit${item.id}`).appendChild(editIcon);
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.id = `delete${item.id}`
+        deleteBtn.type = 'button';
+        deleteBtn.addEventListener("click", () => { deleteContact(item.id) });
+        deleteBtn.classList = "btn btn-outline-danger m-1";
+        document.querySelector(`#${options.id}`).appendChild(deleteBtn);
+        let deleteIcon = document.createElement("i");
+        deleteIcon.classList = "bi bi-trash";
+        document.querySelector(`#delete${item.id}`).appendChild(deleteIcon);
+    })
 }
 
 function favoriteContact(id) {
@@ -186,7 +141,7 @@ function searchContact() {
         contactFilter = contactList.filter(item => item.nome.toLowerCase() == filterName);
         localStorage.setItem('contactFilter', JSON.stringify(contactFilter));
     } else {
-        localStorage.setItem('contactFilter', [0]);
+        contactFilter = localStorage.setItem('contactFilter', [0]);
     }
     location.reload();
 }
@@ -206,11 +161,11 @@ async function updateContact(id) {
         })
     });
     if (response.ok) {
-        console.log("atualizei")  
+        console.log("atualizei")
     } else {
         alert('erro ao atualizar contato');
     }
-    location.reload(); 
+    location.reload();
 }
 
 async function deleteContact(id) {
